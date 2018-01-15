97148730310bcf2bb8e1a2ae009d8c513b70a7fd
Matt Chapman <matt@mttchpmn.com>
2018-01-15 23:49:38 +1300

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install packages
+brew install git
+
+brew install vim
+
+brew install python
+
+brew install python3
+
+brew install zsh
+
+brew install grep
+
+brew install htop
+
+brew install tmux
+
+brew install openssh
+
+brew install node
+
+# Cask for GUI applications
+brew install cask
+
+brew cask install sublime-text
+
+brew-cask install visual-studio-code
+
+brew cask install google-chrome
+
+brew cask install gimp
+
+brew cask install inkscape
+
+brew cask install spotify
+
+brew cask install adobe-creative-cloud
