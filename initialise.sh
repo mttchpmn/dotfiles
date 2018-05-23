b0c74fdd0244c51cd339598366da7a49eb07e481
Matt Chapman <matt@mttchpmn.com>
2018-05-23 17:15:00 +1200

    Refactor init scripot

--- initialise.sh
+++ initialise.sh
@@ -1,92 +1,100 @@
 #!/usr/bin/env bash
 
 setup_packages () {
-    # Update apt
-    sudo apt-get update
+    read -p "Press ENTER to proceed. Press s to skip" ANS
+    if [[ $ANS != s ]]
+    then
+        # Update apt
+        sudo apt-get update
 
-    # Install base packages
-    sudo apt-get install -y git vim tmux tree grep htop openssh nodejs npm python python3 postgresql postgresql-contrib
+        # Install base packages
+        sudo apt-get install -y git vim tmux tree grep htop openssh nodejs npm python python3 postgresql postgresql-contrib
 
-    # Upgrade existing
-    sudo apt-get upgrade
+        # Upgrade existing
+        sudo apt-get upgrade
 
-    # Fix any broken packages / missing dependencies
-    sudo apt-get install -f
+        # Fix any broken packages / missing dependencies
+        sudo apt-get install -f
+    return
+fi
+echo 'Skipping...'
 }
 
 setup_git () {
-    git config --global user.name "Matt Chapman"
-    git config --global user.email "matt@mttchpmn.com"
-    git config --global core.editor vim
-    # Actually *run* the command if we mistype it (2 sec delay)
-    git config --global help.autocorrect 20
+    read -p "Press ENTER to proceed. Press s to skip" ANS
+    if [[ $ANS != s ]]
+    then
+        git config --global user.name "Matt Chapman"
+        git config --global user.email "matt@mttchpmn.com"
+        git config --global core.editor vim
+        # Actually *run* the command if we mistype it (2 sec delay)
+        git config --global help.autocorrect 20
 
-    cd ~/
-    git clone git@github.com:mttchpmn/dotfiles.git
+        cd ~/
+        git clone git@github.com:mttchpmn/dotfiles.git
 
-    cd ~/
-    mkdir code
-    cd code
+        cd ~/
+        mkdir code
+        cd code
 
-    # Wherewolf repositories
-    git clone git@github.com:wherewolfnz/helperscripts.git
-    git clone git@github.com:wherewolfnz/docker.git
-    git clone git@github.com:wherewolfnz/frontend.git
-    git clone git@github.com:wherewolfnz/wherewolf-backend.git
-    git clone git@github.com:wherewolfnz/wherewolf-whitelabel.git
-    git clone git@github.com:wherewolfnz/desmond.git
+        # Wherewolf repositories
+        git clone git@github.com:wherewolfnz/helperscripts.git
+        git clone git@github.com:wherewolfnz/docker.git
+        git clone git@github.com:wherewolfnz/frontend.git
+        git clone git@github.com:wherewolfnz/wherewolf-backend.git
+        git clone git@github.com:wherewolfnz/wherewolf-whitelabel.git
+        git clone git@github.com:wherewolfnz/desmond.git
 
-    # Personal Repositories
-    git clone git@github.com:mttchpmn/MetScope.git
-    git clone git@github.com:mttchpmn/metscope-frontend.git
+        # Personal Repositories
+        git clone git@github.com:mttchpmn/MetScope.git
+        git clone git@github.com:mttchpmn/metscope-frontend.git
+    return
+fi
+echo 'Skipping...'
 }
 
 setup_npm () {
-    sudo npm install -g npm
-    sudo npm install -g eslint nodemqon sails express ionic @angular/cli
+    read -p "Press ENTER to proceed. Press s to skip" ANS
+    if [[ $ANS != s ]]
+    then
+        sudo npm install -g npm
+        sudo npm install -g eslint nodemqon sails express react ionic @angular/cli
+    return
+fi
+echo 'Skipping...'
 }
 
 setup_dotfiles () {
-    ln -s ~/dotfiles/.bashrc ~/.bashrc
-    ln -s ~/dotfiles/.bash ~/.bash
+    read -p "Press ENTER to proceed. Press s to skip" ANS
+    if [[ $ANS != s ]]
+    then
+        ln -s ~/dotfiles/.bashrc ~/.bashrc
+        ln -s ~/dotfiles/.bash ~/.bash
 
-    ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-    ln -s ~/dotfiles/.vim/.vimrc ~/.vimrc
+        ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+        ln -s ~/dotfiles/.vim/.vimrc ~/.vimrc
 
-    source ~/.bashrc
+        source ~/.bashrc
+    return
+fi
+echo 'Skipping...'
 }
 
-checkThen () {
-read -p "Press anything to continue or s to skip" ANS
-if [[ $ANS != s ]]
-then
-    echo 'Proceeding...'
-    $($1)
-fi 
-if [[ $ANS = s ]]
-then
-    echo 'Skipping...'
-fi 
-}
-
-foo () {
-    echo "Blah"
-}
 
 ###################################################################################
 
 echo "Initialising new system setup"
 
 echo "Setting up system packages"
-checkThen setup_packages
+setup_packages
 
 echo "Setting up Git"
-checkThen setup_git
+setup_git
 
 echo "Setting up NPM"
-checkThen setup_npm
+setup_npm
 
 echo "Setting up dotfiles"
-checkThen setup_dotfiles
+setup_dotfiles
 
 echo "All done - enjoy!"
