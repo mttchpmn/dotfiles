5809d9e26cae3751cb4791c3e0bcad096c68d01b
Matt Chapman <matt@mattchapman.io>
2022-02-17 20:21:44 +1300

    Update bootstrapper

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -12,18 +12,6 @@ function Test-CommandExists([string] $commandName) {
 	}
 }
 
-function Open-WebDownloads([string] $configPath) {
-	Write-Host -ForegroundColor Green "Opening web download links..."
-	$config = Get-Content -Path $configPath | ConvertFrom-Json
-
-	foreach ($url in $config.downloads) {
-		Start-Process $url
-	}
-
-	Write-Host "Press [Enter] to continue, once downloads are installed."
-	Read-Host
-}
-
 function Install-Packages([string] $configPath) {
 	Write-Host "Installing packages from $configPath"
 
@@ -44,6 +32,18 @@ function Install-Packages([string] $configPath) {
 	}
 }
 
+function Open-WebDownloads([string] $configPath) {
+	Write-Host -ForegroundColor Green "Opening web download links..."
+	$config = Get-Content -Path $configPath | ConvertFrom-Json
+
+	foreach ($url in $config.downloads) {
+		Start-Process $url
+	}
+
+	Write-Host "Press [Enter] to continue, once downloads are installed."
+	Read-Host
+}
+
 function Link-Files([string] $configPath) {
 	Write-Host -ForegroundColor blue "Symlinking files to home directory..."
 	$config = Get-Content -Path $configPath | ConvertFrom-Json
@@ -60,18 +60,125 @@ function Link-Files([string] $configPath) {
 
 		New-Item -ItemType SymbolicLink -Force -Path $link -Target $original | Out-Null
 	}
-	Write-Host -ForegroundColor Green "Done."
+}
+
+function Install-Custom-Modules([string] $configPath) {
+	Write-Host -ForegroundColor blue "Install Powershell modules..."
+	$config = Get-Content -Path $configPath | ConvertFrom-Json
+
+	foreach ($module in $config.modules) {
+		Write-Host -ForegroundColor Green "Installing module $module..."
+		Install-Module -Force -AllowClobber "$module"
+	}
+}
+
+function Run-Arbitrary-Commands([string] $configPath) {
+	Write-Host -ForegroundColor blue "Running arbitrary commands..."
+	$config = Get-Content -Path $configPath | ConvertFrom-Json
+
+	foreach ($cmd in $config.commands) {
+		Write-Host -ForegroundColor Green "Executing '$cmd'..."
+		Invoke-Expression $cmd
+	}
+}
+
+function Show-UI() {
+	$shouldExit = $false
+
+	while (-Not $shouldExit) {
+		
+		Clear-Host
+		Write-Host -ForegroundColor Green "----------------| BOOTSTRAPPER |---------------"
+		Write-Host ""
+
+		Write-Host "Please select an action from the options below:"
+
+		Write-Host "1) Install packages"
+		Write-Host "2) Open web download links"
+		Write-Host "3) Symlink dotfiles"
+		Write-Host "4) Install PowerShell modules"
+		Write-Host "5) Run arbitrary commands"
+		Write-Host "6) Run all steps"
+		Write-Host ""
+		Write-Host "Q) Quit Bootstrapper"
+		Write-Host "------------------------------------------------"
+
+		$selection = Read-Host -Prompt ":"
+
+		$configPath = "$PSScriptRoot\windows\config.json"
+	
+		Clear-Host
+
+		switch ($selection) {
+			"1" {
+				Install-Packages($configPath)
+
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Done. Press any key to continue"
+				Read-Host
+			}
+			"2" {
+				Open-WebDownloads($configPath)
+
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Done. Press any key to continue"
+				Read-Host
+			}
+			"3" {
+				Link-Files($configPath)
+
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Done. Press any key to continue"
+				Read-Host
+			}
+			"4" {
+				Install-Custom-Modules($configPath)
+
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Done. Press any key to continue"
+				Read-Host
+			}
+			"5" {
+				Run-Arbitrary-Commands($configPath)
+
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Done. Press any key to continue"
+				Read-Host
+			}
+			"6" {
+				Install-Packages($configPath)
+				Open-WebDownloads($configPath)
+				Link-Files($configPath)
+				Install-Custom-Modules($configPath)
+				Run-Arbitrary-Commands($configPath)
+				
+				Write-Host ""
+				Write-Host "------------------------------------------------"
+				Write-Host -ForegroundColor Green "Bootstrapping complete. Have a nice day."
+				$shouldExit = $true
+
+			}
+			"Q" {
+				$shouldExit = $true
+			}
+			"q" {
+				$shouldExit = $true
+			}
+			Default {
+				Write-Host -ForegroundColor Red "Input not recognised. Exiting"
+				$shouldExit = $true
+			}
+		}
+
+	}
 }
 
 function Main {
-	$configPath = ".\windows\config.json"
-
-	Write-Host -ForegroundColor Blue "Bootstrapping Windows machine..."
-
-	# Install-Applications($configPath)
-	# Open-WebDownloads($configPath)
-	Link-Files($configPath)
-
-	Write-Host -ForegroundColor Green "Bootstrapping complete. Have a nice day."
+	Show-UI
 }
 Main
